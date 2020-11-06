6b1f9c63601652e18982fd85024bb93c7a25c396
mfrancoo <73757516+mfrancoo@users.noreply.github.com>
2020-11-06 14:49:24 -0500

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 public class Menu{
     public void mostrarMenu(){
-      Usuario u1 =new Usuario();
+      Scanner scan = new Scanner(System.in);
       System.out.println("Bienvenido al programa,por favor ingrese los datos solicitados");
-      System.out.println("Nombre, ocupación, meta de tiempo de uso del computador");
-      
+      System.out.println("Nombre");
+      String n= scan.next();
+      System.out.println("Ocupación");
+      String o= scan.next();
+      System.out.println("Meta de uso");
+      int m=scan.nextInt();
+      Usuario u = new Usuario(n,o,m);
     }
     Public void generarBarras(){
     
