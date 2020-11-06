b2de6839e475cdc3715def36a1dddbfed5a7ec5a
mfrancoo <73757516+mfrancoo@users.noreply.github.com>
2020-11-06 14:38:28 -0500

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -3,7 +3,6 @@ public class Menu{
       Usuario u1 =new Usuario();
       System.out.println("Bienvenido al programa,por favor ingrese los datos solicitados");
       System.out.println("Nombre, ocupación, meta de tiempo de uso del computador");
-      u1.ingresarDatosUsuario();
       
     }
     Public void generarBarras(){
