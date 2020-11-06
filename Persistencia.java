26fc266e63eb8409998a86000f5d17f65727b77b
LauraEchavarria <laura.echavarria.pelaez@gmail.com>
2020-11-06 12:03:17 -0500

    .

--- Persistencia.java
+++ Persistencia.java
@@ -4,15 +4,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Persistencia {
-    // almacena en txt el tiempo promedio de cada día
-    String ruta = "/home/mario/archivo.txt";
-    File archivo = new File(ruta);
-    BufferedWriter bw;
-    if(archivo.exists()) {
-        bw = new BufferedWriter(new FileWriter(archivo));
-        bw.write("El fichero de texto ya estaba creado.");
-    } else {
-        bw = new BufferedWriter(new FileWriter(archivo));
-        bw.write("Acabo de crear el fichero de texto.");
+
+    public static void main(String[] args) throws IOException {
+        String ruta = "/home/mario/archivo.txt";
+        File archivo = new File(ruta);
+        BufferedWriter bw;
+        if(archivo.exists()) {
+            bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write("El fichero de texto ya estaba creado.");
+        } else {
+            bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write("Acabo de crear el fichero de texto.");
+        }
+        bw.close();
     }
+    
 }
