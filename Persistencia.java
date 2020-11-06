53f954274df482c1914623f91822da44dd5d5f19
LauraEchavarria <laura.echavarria.pelaez@gmail.com>
2020-11-06 11:58:28 -0500

    .

--- Persistencia.java
+++ Persistencia.java
@@ -1,3 +1,16 @@
-public class Persistencia{
-   
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Persistencia {
+    // almacena en txt el tiempo promedio de cada día
+    String ruta = "/home/mario/archivo.txt";
+    File archivo = new File(ruta);
+    BufferedWriter bw; {}
+    if(archivo.exists()) {
+      // El fichero ya existe
+    } else {
+      // El fichero no existe y hay que crearlo
+    }
 }
