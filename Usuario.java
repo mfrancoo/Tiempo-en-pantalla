185050253cb39896aa018f4cbd15bb5af8a1c5d5
Manuela <m2607fraor@gmail.com>
2020-11-06 11:03:40 -0500

    .

--- Usuario.java
+++ Usuario.java
@@ -1,4 +1,13 @@
-
 public class Usuario{
-    private String nombre;
+        private String nombre;
+        private String ocupacion;
+        private int metaUso;
+        public void ingresarDatosUsuario(String n, String o, int m){
+            this.nombre = n;
+            this.ocupacion = o;
+            this.metaUso = m;
+        }
+        public String getNombre(){
+            return this.nombre;
+        }
 }
