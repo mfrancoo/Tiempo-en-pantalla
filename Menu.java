67c9f83732c1bc5f06131255358154a67142a677
LauraEchavarria <laura.echavarria.pelaez@gmail.com>
2020-11-07 09:52:16 -0500

    .

--- Menu.java
+++ Menu.java
@@ -1,49 +1,49 @@
 import java.util.Scanner;
-public class Menu extends Procesamiento{
-    public void mostrarMenu() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Bienvenido, por favor ingrese los datos solicitados");
-        System.out.println("Nombre:");
-        String nombre = scan.next();
-        System.out.println("Ocupación:");
-        String ocupacion = scan.next();
-        System.out.println("Meta de uso (horas)");
-        int metaUso = scan.nextInt();
-        Usuario usuario = new Usuario(nombre, ocupacion, metaUso);
 
-        Procesamiento.iniciarTiempo();
-        System.out.println("--- Presione x para detener el temporizador y ver sus graficas de uso ---");
+public class Menu extends Procesamiento {
+  public void mostrarMenu() {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Bienvenido, por favor ingrese los datos solicitados");
+    System.out.println("Nombre:");
+    String nombre = scan.next();
+    System.out.println("Ocupación:");
+    String ocupacion = scan.next();
+    System.out.println("Meta de uso (horas)");
+    int metaUso = scan.nextInt();
+    Usuario usuario = new Usuario(nombre, ocupacion, metaUso);
 
-        String finalizar = scan.next().toLowerCase().trim();
-        if (finalizar == "x") {
-            Procesamiento.finalizarTiempo();
-            Procesamiento.calcularPromedioDiario();
-            usuario.mostrarMetaUso();
-            generarBarras("diario");
-        }
+    Procesamiento.iniciarTiempo();
+    System.out.println("--- Presione x para detener el temporizador y ver sus graficas de uso ---");
 
-        scan.close();
+    String finalizar = scan.next().toLowerCase().trim();
+    if (finalizar == "x") {
+      Procesamiento.finalizarTiempo();
+      Procesamiento.calcularPromedioDiario();
+      usuario.mostrarMetaUso();
+      generarBarras("diario");
     }
 
-    public void generarBarras(String tipo) {
-        if (tipo == "diario") {
-            System.out.println("Uso diario");
-            for (int i = 8; i > 0; i--) {
-                System.out.println("  "+i+"  ");
-                for (int j = 8; j > 0; j--) {
-                    if (Procesamiento.promediosDiarios.get(promediosDiarios.size()-1) >= i) {
-                        System.out.print("|////|");
-                    } else {
-                        System.out.print("|    |");
-                    }
-                }
-            }
-            System.out.println("  h   Uso");
+    scan.close();
+  }
 
-        } else if (tipo == "semanal") {
-
-        } else if (tipo == "mensual") {
+  public void generarBarras(String tipo) {
+    if (tipo == "diario") {
+      System.out.println("Uso diario");
+      for (int i = 8; i > 0; i--) {
+        System.out.println("  " + i + "  ");
+        for (int j = 8; j > 0; j--) {
+          if (Procesamiento.promediosDiarios.get(promediosDiarios.size() - 1) >= i) {
+            System.out.print("|////|");
+          } else {
+            System.out.print("|    |");
+          }
         }
+      }
+      System.out.println("  h   Uso");
+
+    } else if (tipo == "semanal") {
+
+    } else if (tipo == "mensual") {
     }
+  }
 }
-
