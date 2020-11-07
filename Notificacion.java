9a2e24b33128f949848aaa2c15a41421f239eae4
LauraEchavarria <laura.echavarria.pelaez@gmail.com>
2020-11-07 09:13:19 -0500

    .

--- Notificacion.java
+++ Notificacion.java
@@ -6,7 +6,6 @@ public class Notificacion extends Usuario{
     private String tipoPausa1 = this.getNombre()+", llevas media hora en pantalla, es momento de pararse, estirar y darse un respiro";
     private String tipoPausa2 = this.getNombre()+", llevas una hora en pantalla, es momento de pararse, estirar y darse un respiro" ;
 
-<<<<<<< HEAD
     public void mostrarPausa(){
         if(){
         
@@ -21,9 +20,7 @@ public class Notificacion extends Usuario{
             
         }    
     }
-=======
     //public String mostrarPausa(){}
 
->>>>>>> a1a834b72f93038254e6bcf6f6916c3de3b221dd
 }
 
