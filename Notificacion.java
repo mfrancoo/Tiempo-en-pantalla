22ff5a39212c8b91722a5d30503293f2387f55d0
Manuela Franco Orozco <m2607fraor@gmail.com>
2020-11-07 09:26:10 -0500

    .

--- Notificacion.java
+++ Notificacion.java
@@ -6,7 +6,7 @@ public class Notificacion extends Usuario{
     private String tipoPausa1 = this.getNombre()+", llevas media hora en pantalla, es momento de pararse, estirar y darse un respiro";
     
     public void mostrarPausa() {
-    for(int i=0; i<(Usuario.getMetaUso()*60); i+=30){
+    for(int i=0; i<(this.getMetaUso()*60); i+=30){
                 LocalDateTime tpausa = Procesamiento.tInicio.plus(i, ChronoUnit.MINUTES);
         if (Procesamiento.tInicio.isEqual(tpausa)) {
             System.out.println(tipoPausa1);
