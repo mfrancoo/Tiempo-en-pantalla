aa04d5d7f15c47b4cc7f84d3892e3340013e5bd7
LauraEchavarria <laura.echavarria.pelaez@gmail.com>
2020-11-07 11:05:04 -0500

    .

--- Notificacion.java
+++ Notificacion.java
@@ -3,10 +3,10 @@ import java.time.temporal.ChronoUnit;
 
 public class Notificacion extends Usuario {
 
-    private String tipoPausa1 = this.getNombre() + ", llevas media hora en pantalla, es momento de pararse, estirar y darse un respiro";
+    public static String tipoPausa1 = Usuario.nombreCopia + ", llevas media hora en pantalla, es momento de pararse, estirar y darse un respiro";
 
-    public void mostrarPausa() {
-        for (int i = 0; i < (this.getMetaUso() * 60); i += 30) {
+    public static void mostrarPausa() {
+        for (int i = 0; i < (Usuario.metaUsoCopia * 60); i += 30) {
             LocalDateTime tpausa = Procesamiento.tInicio.plus(i, ChronoUnit.MINUTES);
             if (Procesamiento.tInicio.isEqual(tpausa)) {
                 System.out.println(tipoPausa1);
