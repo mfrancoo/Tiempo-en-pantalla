3f3a814a74c4e5f3a12e6a3c87de3950fe4ca6ee
LauraEchavarria <laura.echavarria.pelaez@gmail.com>
2020-11-07 09:11:35 -0500

    .

--- Notificacion.java
+++ Notificacion.java
@@ -1,13 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class Notificacion extends Usuario{
     
     private String tipoPausa1 = this.getNombre()+", llevas media hora en pantalla, es momento de pararse, estirar y darse un respiro";
     private String tipoPausa2 = this.getNombre()+", llevas una hora en pantalla, es momento de pararse, estirar y darse un respiro" ;
 
-    /*public String mostrarPausa(){
+    public void mostrarPausa(){
         if(){
         
         }else if(){
         
         }
-    }*/
+    }
+
+    public void experimento() {
+        LocalDateTime tpausa = Procesamiento.tInicio.plus(30, ChronoUnit.MINUTES);
+        if (Procesamiento.tInicio.isEqual(tpausa)) {
+            
+        }    
+    }
 }
