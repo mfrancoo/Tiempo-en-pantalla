9dc00133373eca288bc665f5a9a25e946fdd1156
LauraEchavarria <laura.echavarria.pelaez@gmail.com>
2020-11-25 12:37:47 -0500

    .

--- Persistencia.java
+++ Persistencia.java
@@ -6,7 +6,7 @@ public class Persistencia {
 
     public static void escribirAFichero(String datos) {
 
-        File file = new File("RegistroSesionesMinutes.txt");
+        File file = new File("RegistroSesionesMinutos.txt");
         PrintWriter output = null;
         try {
             output = new PrintWriter(new FileWriter(file, true));
